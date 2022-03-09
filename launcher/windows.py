69506cc9e754e5c875dcc538c347364af7dddfc6
Andrew Delph <andrewdelph@live.ca>
2022-03-09 08:33:29 -0500

    improvements to the launcher

--- launcher/windows.py
+++ launcher/windows.py
@@ -3,28 +3,33 @@ import sys
 from clint.textui import progress
 from datetime import datetime
 import os
+import zipfile
 
 def download_file(url, file_name):
     r = requests.get(url, stream=True)
     last_modfied = None
     with open(file_name, 'wb') as f:
-        print(r.headers)
-        last_modfied = r.headers['Last-Modified']
         total_length = int(r.headers.get('content-length'))
         for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
             if chunk:
                 f.write(chunk)
                 f.flush()
-    return last_modfied
 
 
 url = 'https://thing-72-game.s3.amazonaws.com/game.zip'
 file_name = "game.zip"
 
-download_date = download_file(url, file_name)
-print(download_date)
-local_date = os.path.getmtime(file_name)
-local_date = datetime.fromtimestamp(local_date).strftime('%Y-%m-%d %H:%M:%S')
-print(local_date)
+
+download_file(url,file_name)
+
+# unzip the file
+try:
+    with zipfile.ZipFile(file_name, 'r') as zip_ref:
+        zip_ref.extractall("unzip")
+except:
+    print("folder already there")
+
+# exec the file
+os.startfile("unzip\myapp.exe")
 
 print('hi')
