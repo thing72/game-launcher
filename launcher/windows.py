7682e479df548c94ff63d960d870ea32bf8ce61a
Andrew Delph <andrewdelph@live.ca>
2022-03-08 22:26:36 -0500

    compare dates

--- launcher/windows.py
+++ launcher/windows.py
@@ -1,39 +1,30 @@
 import requests
 import sys
 from clint.textui import progress
-
-# def download_file(link, file_name):
-#     with open(file_name, "wb") as f:
-#         print("Downloading %s" % file_name)
-#         response = requests.get(link, stream=True)
-#         total_length = response.headers.get('content-length')
-
-#         if total_length is None: # no content length header
-#             f.write(response.content)
-#         else:
-#             dl = 0
-#             total_length = int(total_length)
-#             for data in response.iter_content(chunk_size=4096):
-#                 dl += len(data)
-#                 f.write(data)
-#                 done = int(50 * dl / total_length)
-#                 sys.stdout.write("\r[%s%s]" % ('=' * done, ' ' * (50-done)) )    
-#                 sys.stdout.flush()
-#     print()
+from datetime import datetime
+import os
 
 def download_file(url, file_name):
     r = requests.get(url, stream=True)
+    last_modfied = None
     with open(file_name, 'wb') as f:
+        print(r.headers)
+        last_modfied = r.headers['Last-Modified']
         total_length = int(r.headers.get('content-length'))
         for chunk in progress.bar(r.iter_content(chunk_size=1024), expected_size=(total_length/1024) + 1): 
             if chunk:
                 f.write(chunk)
                 f.flush()
+    return last_modfied
 
 
 url = 'https://thing-72-game.s3.amazonaws.com/game.zip'
 file_name = "game.zip"
 
-download_file(url, file_name)
+download_date = download_file(url, file_name)
+print(download_date)
+local_date = os.path.getmtime(file_name)
+local_date = datetime.fromtimestamp(local_date).strftime('%Y-%m-%d %H:%M:%S')
+print(local_date)
 
 print('hi')
